1fafef9c9c14809d9ddd9bf1a166e80c7cdb5430
joao <joaoghabriel02@gmail.com>
2023-10-24 11:11:47 -0300

    cep struct

--- controllers/ceps.go
+++ controllers/ceps.go
@@ -9,7 +9,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Cep struct {
+	Cep         string `json:"cep,omitempty"`
+	Logradouro  string `json:"logradouro,omitempty"`
+	Complemento string `json:"compleme nto,omitempty"`
+	Bairro      string `json:"bairro,omitempty"`
+	Localidade  string `json:"localidade,omitempty"`
+	UF          string `json:"uf,omitempty"`
+	IBGE        string `json:"ibge,omitempty"`
+	GIA         string `json:"gia,omitempty"`
+	DDD         string `json:"ddd,omitempty"`
+	SIAFI       string `json:"siafi omitempty"`
+}
+
 func GetCepData(c *gin.Context) {
+
+	var Cep Cep
+
 	response, err := http.Get("https://viacep.com.br/ws/01001000/json/")
 	if err != nil {
 		helpers.Error(c, http.StatusInternalServerError, err)
